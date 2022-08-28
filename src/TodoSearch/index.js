2d5087b5536144704873880ab9cfee5ec0e3df46
cristian ortega <cjoruiz@hotmail.es>
2022-08-28 17:46:54 -0500

    placeholder del search

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -13,7 +13,7 @@ function TodoSearch() {
   return (
     <input
       className="TodoSearch"
-      placeholder="Cebolla"
+      placeholder="Escribe la tarea a realizar"
       value={searchValue}
       onChange={onSearchValueChange}
     />
